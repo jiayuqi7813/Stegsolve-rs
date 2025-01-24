d04d13af673918ca9cd6c631efefbf7ce67e7572
jiayuqi7813 <1783671926@qq.com>
2025-01-24 20:06:24 +0800

    fix bug

--- mac_build.sh
+++ mac_build.sh
@@ -12,7 +12,6 @@ cargo bundle --release
 # 确保 Frameworks 目录存在
 mkdir -p "${FRAMEWORKS_PATH}"
 
-ls target/release/bundle/osx/Stegsolve.app/Contents/MacOS/
 
 if [[ ! -f "${BINARY_PATH}" ]]; then
     echo "错误：未找到主程序 ${BINARY_PATH}，请检查路径或构建过程！"
